29b11b9db814c0a07a12e0ca32a5789007f0f361
Kevin Cogen <k.cogen@gmail.com>
2023-08-16 14:36:47 -0400

    refactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,21 @@
 const request = require('request');
-const breed = process.argv[2];
-request(`https://api.thecatapi.com/v1/breeds/search/?q=${breed}`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else if (!error) {
-    const data = JSON.parse(body);
-    if (data[0] === undefined) {
-      console.log("We couldn't find that Breed of cat!");
-    } else if (data[0] !== undefined) {
-      console.log(data[0].name);
-      console.log(data[0].description);
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search/?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null); 
+      return;
     }
-  }
-});
+
+    const data = JSON.parse(body);
+    
+    if (data[0] === undefined) {
+      callback("We couldn't find that Breed of cat!", null);
+      return;
+    }
+
+    callback(null, data[0].description); 
+  });
+};
+
+module.exports = { fetchBreedDescription };
